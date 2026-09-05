8b16efc340676427feecd3f4ba510b0b12e2ce40
GuruB-Codes <gurulingappaofficial@gmail.com>
2026-09-05 18:00:00 +0530

    Even or odd

---
+++ Loops/Even_or_Odd.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int number;cin>>number;
+  if(number%2==0){
+    cout<<"Even number";
+  }else{
+    cout<<"Odd number";
+  }
+
+    return 0;
+}
