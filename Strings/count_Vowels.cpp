6eeaebc98c29a2ee5f1acc7531f0253ef73db319
GuruB-Codes <gurulingappaofficial@gmail.com>
2026-09-06 20:59:47 +0530

    Count vowels in string

---
+++ Strings/count_Vowels.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   string s; cin>> s;
+  int cnt=0;
+  for(char c:s){
+    c=tolower(c);
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+        cnt++;
+    }
+  }cout<<cnt;
+
+    return 0;
+}
